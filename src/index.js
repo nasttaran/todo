fd6fafd41295883af4f73ad078564cdbaa6c5e47
nastianm <nastaran.mans86@gmail.com>
2022-09-13 13:09:05 +0430

    commit
    Signed-off-by: nastianm <nastaran.mans86@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createstore} from 'redux';
+import todoslice from './redux/slices/todoslice';
+
+const store = createstore(todoslice)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
